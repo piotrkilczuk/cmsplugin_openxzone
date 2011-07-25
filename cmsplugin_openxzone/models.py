53030e6a31032d8ff761d7bfc03ee1514e8d86e2
centralniak <p.kilczuk@stylesheet.pl>
2011-07-25 17:30:38 +0200

    Add support for floating banner zones

--- cmsplugin_openxzone/models.py
+++ cmsplugin_openxzone/models.py
@@ -2,6 +2,12 @@ from cms.models import CMSPlugin
 from django.db import models
 from django_openx.zone import Zone
 
+# see: /lib/OA/Dll/Zone.php
+ZONE_TYPE_MAPPING = (
+    'adjs',
+    'adlayer',
+)
+
 
 class OpenXZonePlugin(CMSPlugin):
     zone_id = models.PositiveSmallIntegerField()
@@ -18,22 +24,23 @@ class OpenXZonePlugin(CMSPlugin):
         return Zone.get(self.zone_id)
     
     def __get_tag(self):
-        return str(Zone.get(self.zone_id).generate_tag())
-        #        try:
-        #            js = self.get_zone_dict().generate_tag()
-        #        except AttributeError:
-        #            js = ''
-        #        return js
-    
+        # no idea why isn't the proper code returned, when requesting
+        # generate_tag WITHOUT code_type
+        try:
+            code_type = ZONE_TYPE_MAPPING[self.type]
+        except IndexError:
+            code_type = ZONE_TYPE_MAPPING[0]
+        return str(Zone.get(self.zone_id).generate_tag(code_type=code_type))
+
     def __unicode__(self):
         return 'Zone ' + str(self.zone_id)
     
     def save(self, *args, **kwargs):
         zone_dict = self.__get_zone_dict()
-        self.tag = self.__get_tag()
         self.height = zone_dict['height']
         self.publisher_id = zone_dict['publisher_id']
         self.type = zone_dict['type']
         self.width = zone_dict['width']
         self.zone_name = zone_dict['zone_name']
+        self.tag = self.__get_tag()
         super(OpenXZonePlugin, self).save(*args, **kwargs)
